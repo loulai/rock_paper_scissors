49468febda2ce965d8c05edda634a426f5770b2f
Louise <louiselai88@gmail.com>
2014-05-12 14:31:08 +0100

    all specs pass

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,6 +1,8 @@
-var PAIRS = function {
-	
-};
+PAIRS = {
+	'rock' : {'beats': 'scissors'},
+	'paper' : {'beats':'rock'},
+	'scissors' : {'beats':'paper'}
+}
 
 function Player(name) {
   this.name = name;
@@ -16,6 +18,13 @@ function Game(player1, player2) {
 };
 
 Game.prototype.winner = function() {
-  
-  return this.player2;
+  if(this.player1.pick === this.player2.pick) {
+  	return null;
+  }
+  else if(PAIRS[this.player1.pick]['beats'] === this.player2.pick){
+  	return this.player1;
+  }
+
+  else 
+  	return this.player2;
 };
